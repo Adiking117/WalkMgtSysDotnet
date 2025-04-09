d04f22b5f00afbb9fea94956ba377f8ae366e0a7
[Aditya <[www.aditya117@gmail.com]>
2025-04-09 20:34:17 +0530

    setup files with DB context

---
+++ Data/WalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WalkManagementSystem.Models.Domain;
+
+namespace WalkManagementSystem.Data
+{
+    public class WalksDBContext : DbContext
+    {
+        public WalksDBContext(DbContextOptions<WalksDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Difficulty> Difficulty { get; set; }
+    }
+}
